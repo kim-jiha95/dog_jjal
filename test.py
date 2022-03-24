2531f5757c005aa05980bd70ea57cfe9a0a3a4ab
kim-jiha95 <kimjiha1112@gmail.com>
2022-03-25 01:18:03 +0900

    list

---
+++ test.py
@@ -0,0 +1,8 @@
+def my_letter():
+        return ("please input valid result")
+
+        my_letter = str(input("Please type down letter"))
+
+        while my_letter != "letter" and user
+c = my_letter()
+print(c)
